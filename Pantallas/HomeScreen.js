033bc214e0dc2112302e2e512ad1822f79dc5c11
diegovinals98 <diego.vinalslage@gmail.com>
2024-09-10 16:28:19 +0200

    Ahora si entras te lleva directo a un grupo

--- Pantallas/HomeScreen.js
+++ Pantallas/HomeScreen.js
@@ -11,8 +11,6 @@ import {
   TextInput, 
   TouchableWithoutFeedback,
   FlatList,
-  Keyboard,
-  Button,
   Alert,
   ScrollView,
   RefreshControl,
@@ -23,19 +21,13 @@ import { useNavigation, useRoute } from '@react-navigation/native';
 import { useUser } from '../userContext.js'; // Importa el contexto del usuario.
 import { StatusBar } from 'expo-status-bar';
 import { globalStyles } from '../estilosGlobales.js'; // Importa estilos globales.
-import { SelectCountry, Dropdown } from 'react-native-element-dropdown';
-import AntDesign from '@expo/vector-icons/AntDesign';
+import { Dropdown } from 'react-native-element-dropdown';
 import { useFocusEffect } from '@react-navigation/native';
-import DropDownPicker from 'react-native-dropdown-picker';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
-import * as Notifications from 'expo-notifications';
-import Constants from 'expo-constants';
-import { set } from 'lodash';
 
 // Obtiene las dimensiones de la ventana del dispositivo.
 const windowHeight = Dimensions.get('window').height;
 
-// Componente principal de la pantalla de inicio.
 const HomeScreen = () => {
   // Hook de navegación y rutas de react-navigation.
   const route = useRoute();
@@ -55,90 +47,74 @@ const HomeScreen = () => {
   const iniciales = user?.nombre ? `${user?.nombre.charAt(0)}${user?.apellidos.charAt(0)}` : '';
 
   // Estados del componente.
-  const [data, setData] = useState([]); // Estado para datos de usuarios.
-  const [seriesData, setSeriesData] = useState([]); // Estado para datos generales de series.
-  const [serieDetalle, setSerieDetalle] = useState([]); // Estado para detalles específicos de una serie.
-  const [seriesIds, setseriesIds] = useState([]);
   const [seriesDetalles, setSeriesDetalles] = useState([]);
   const [idelegido, setIdElegido] = useState();
+  const [grupoInicialSeleccionado, setGrupoInicialSeleccionado] = useState(false); // Controla si el grupo inicial ya fue seleccionado
 
   // Estado para la visibilidad del menú desplegable y el grupo seleccionado.
   const [TodosGrupos, setTodosGrupos] = useState([]); // Estado para almacenar todos los grupos.
-  const [value, setValue] = useState("Grupos"); // variable que almacena en que grupo estamos
+  const [value, setValue] = useState(null); // Inicialmente sin valor hasta cargar grupos.
   const [isFocus, setIsFocus] = useState(false);
   const [refrescar, setRefrescar] = useState(false);
   const [refrescando, setRefrescando] = useState(false);
 
-  const [open, setOpen] = useState(false);
-  const [items, setItems] = useState(TodosGrupos.map(grupo => ({ label: grupo.Nombre_grupo, value: grupo.Nombre_grupo })));
-  const [ordenAscendente, setOrdenAscendente] = useState(true);
-
+  // Ejecuta al cargar la pantalla por primera vez
   useFocusEffect(
     React.useCallback(() => {
-      // Llama a las funciones que cargan los datos
-      console.log('---------------------------------------- HOME SCREEN ----------------------------------------');
-      llamarAGrupos();
-      obtenerSeries();
-      resetearBusqueda();
-      setRefrescar(prev => !prev);
-
-    }, [])
+      if (!grupoInicialSeleccionado) {
+        llamarAGrupos(); // Solo se llama al cargar la primera vez
+      }
+    }, [grupoInicialSeleccionado]) // Solo depende de si se ha seleccionado el grupo inicial
   );
 
   const onRefresh = React.useCallback(() => {
     setRefrescando(true);
-    // Aquí debes llamar a las funciones que actualizan tus datos
     resetearBusqueda();
     setRefrescar(prev => !prev);
-    llamarAGrupos();
-    obtenerSeries();
-    console.log(' ---------------------- ID elegido ----------------------' + idelegido)
-
+    obtenerSeries(); // Solo actualizará series, no selecciona el primer grupo nuevamente
     setRefrescando(false);
-  }, []);
-
-  // Función para manejar la selección de un grupo.
-  const handleSelectItem = (item) => {
-    setSelectedItem(item.Nombre_grupo);
-    setIsVisible(false);
-  };
-
-  async function anadirGrupo() {
-    navigation.navigate('Añadir Grupo')
-  }
+  }, [value, idelegido]); // Evita que se pierda la selección actual del grupo
 
   // Función para realizar la llamada a la API y obtener los grupos del Usuario
-  const llamarAGrupos = () => {
-    console.log('Entrado en llamarGrupos')
-    fetch(`https://apitfg.lapspartbox.com/grupos/${user?.id}`)
-      .then((response) => response.json())
-      .then((json) => setTodosGrupos(json))
-      .catch((error) => console.error('Error al obtener los grupos:', error));
-    console.log("Grupos del Usuario: " + user.nombre + user.apellidos);
-    console.log(TodosGrupos);
+  const llamarAGrupos = async () => {
+    try {
+      console.log('Entrando en llamarGrupos');
+      const response = await fetch(`https://apitfg.lapspartbox.com/grupos/${user?.id}`);
+      const json = await response.json();
+      setTodosGrupos(json);
 
-  }
+      // Si hay grupos y el grupo inicial aún no fue seleccionado
+      if (json.length > 0 && !grupoInicialSeleccionado) {
+        console.log("Solo debería aparecer una vez");
+        setValue(json[0].Nombre_grupo);
+        setIdElegido(json[0].ID_Grupo);
+        setGrupoInicialSeleccionado(true); // Marca que el grupo inicial ya fue seleccionado
+      } else if (json.length == 0){
+        setValue("Grupos");
+      }
+
+      console.log("Grupos del Usuario: " + user.nombre + " " + user.apellidos);
+      console.log(json);
+    } catch (error) {
+      console.error('Error al obtener los grupos:', error);
+    }
+  };
 
   useEffect(() => {
-    console.log('El ID elegido es ahora: ' + idelegido);
-    obtenerSeries();
-    // Cualquier otra acción que necesite el estado actualizado
+    if (idelegido) {
+      console.log('El ID elegido es ahora: ' + idelegido);
+      obtenerSeries();
+    }
   }, [idelegido]);
 
   const obtenerSeriesDelUsuario = async (userId, nombre, idgrupo) => {
-    console.log('Obtener series del usuario con id: ' + userId);
-    console.log('Obtener series del grupo con id: ' + idgrupo);
-    console.log('Obtener series del grupo con nombre: ' + nombre);
     try {
       const url = new URL(`https://apitfg.lapspartbox.com/series-ids-usuario/${userId}/${idgrupo}`);
-
-      // Llamada al endpoint con userId y value como parámetros de consulta
       const respuesta = await fetch(url);
       if (!respuesta.ok) {
         throw new Error('Respuesta de red no fue ok.');
       }
       const seriesIds = await respuesta.json();
-      console.log('Series: ' + seriesIds)
       return seriesIds;
     } catch (error) {
       console.error('Hubo un problema con la petición fetch:', error);
@@ -146,39 +122,36 @@ const HomeScreen = () => {
   };
 
   const obtenerSeries = () => {
-
-    if (value == 'Grupos') {
-      console.log('Estamos en grupos, por lo que no hay series')
-      setSeriesDetalles([])
-    } else {
-      obtenerSeriesDelUsuario(user.id, value, idelegido).then(seriesIds => {
-        // Verifica si seriesIds está vacío
-        if (seriesIds.length === 0) {
-          console.log('No hay series para mostrar');
-          return; // Sale de la función si no hay IDs de series
-        }
-
-        // Si seriesIds no está vacío, ejecuta el resto del código
-        Promise.all(seriesIds.map(serieID =>
-          fetch(`https://api.themoviedb.org/3/tv/${serieID}?api_key=c51082efa7d62553e4c05812ebf6040e&language=es-ES`)
-            .then(response => response.json())
-        )).then(seriesDetalles => {
-          setSeriesDetalles(seriesDetalles); // Guardar los detalles de las series en el estado
-        }).catch(error => console.error('Error:', error));
-      });
+    if (!value) {
+      console.log('No hay grupos seleccionados.');
+      setSeriesDetalles([]);
+      return;
     }
+    obtenerSeriesDelUsuario(user.id, value, idelegido).then(seriesIds => {
+      if (seriesIds.length === 0) {
+        console.log('No hay series para mostrar');
+        return;
+      }
+      Promise.all(seriesIds.map(serieID =>
+        fetch(`https://api.themoviedb.org/3/tv/${serieID}?api_key=c51082efa7d62553e4c05812ebf6040e&language=es-ES`)
+          .then(response => response.json())
+      )).then(seriesDetalles => {
+        setSeriesDetalles(seriesDetalles);
+      }).catch(error => console.error('Error:', error));
+    });
+  };
 
-  }
-
-  // Efecto para cargar datos de una serie específica.
   useEffect(() => {
     llamarAGrupos();
     obtenerSeries();
   }, [refrescar]);
 
-  // Función para navegar a la pantalla de ajustes.
   const handleSettings = () => {
     navigation.navigate('Settings');
+  };
+
+  async function anadirGrupo() {
+    navigation.navigate('Añadir Grupo')
   }
 
   const poster = (path) => {
@@ -192,14 +165,14 @@ const HomeScreen = () => {
         style={styles.poster}
       />
     );
-  }
+  };
 
   const [query, setQuery] = useState('');
   const [series, setSeries] = useState([]);
 
   const handleTextChange = (text) => {
     setQuery(text);
-    buscarSeries()
+    buscarSeries();
   };
 
   const buscarSeries = async () => {
@@ -239,7 +212,6 @@ const HomeScreen = () => {
   };
 
   const seleccionSerie = (text, idSerie) => {
-    console.log("Se quire añadir " + text + ', con el id: ' + idSerie)
     Alert.alert(
       'Confirmación',
       `¿Estás seguro de que quieres añadir la serie: ${text}?`,
@@ -247,7 +219,7 @@ const HomeScreen = () => {
         {
           text: 'Sí',
           onPress: async () => {
-            agregarSerieAUsuario(user.id, idSerie)
+            agregarSerieAUsuario(user.id, idSerie);
             resetearBusqueda();
             setRefrescar(prev => !prev);
           },
@@ -275,11 +247,11 @@ const HomeScreen = () => {
 
   const editarGrupo = (nombreGrupo) => {
     navigation.navigate('Editar Grupo', { idelegido, nombreGrupo });
-  }
+  };
 
   const verCalendario = (nombreGrupo) => {
     navigation.navigate('Calendario', { nombreGrupo, idelegido });
-  }
+  };
 
   const ordenarSeriesPorTitulo = () => {
     const seriesOrdenadas = [...seriesDetalles].sort((a, b) => {
